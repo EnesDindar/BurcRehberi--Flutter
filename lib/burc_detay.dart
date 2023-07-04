d607ce886ab380e9a921b99719d6b510ed7cf0e6
Enes Dindar <enesdindarr@gmail.com>
2023-07-04 15:18:53 +0300

    3. commit

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/model/burc.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   final Burc secilenBurc;
   const BurcDetay({required this.secilenBurc, super.key});
 
   @override
+  State<BurcDetay> createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Color appbarRengi = Colors.transparent;
+  late PaletteGenerator _generator;
+  @override
+  void initState() {
+    appbarRenginiBul();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(), body: CustomScrollView());
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.arrow_back_ios_new, size: 40, color: Colors.white),
+          ),
+          expandedHeight: 250,
+          pinned: true,
+          backgroundColor: appbarRengi,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(widget.secilenBurc.burcAdi + ' Burcu ve Özellikleri'),
+            centerTitle: true,
+            background: Image.asset(
+              'images/' + widget.secilenBurc.burcBuyukResim,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.all(8),
+            child: SingleChildScrollView(
+              child: Text(
+                widget.secilenBurc.burcDetayi,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+
+  void appbarRenginiBul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+        AssetImage('images/${widget.secilenBurc.burcBuyukResim}'));
+    appbarRengi = _generator.dominantColor!.color;
+    setState(() {});
   }
 }
